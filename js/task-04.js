6e53a1ce84cbf99ecf396750e79a65f96f49d19a
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2023-12-08 23:21:51 +0100

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+
+const register = {};
+
+loginForm.addEventListener("submit", enter => {
+    enter.preventDefault();
+    const data = new FormData(enter.target);
+    const emailValue = loginForm.elements.email.value.trim();
+    const passwordValue = loginForm.elements.password.value.trim();
+
+    if (!emailValue || !passwordValue) {
+        alert("All form fields must be filled in!");
+    } else {
+        data.forEach((value, key) => {
+            register[key] = value;
+        });
+
+        console.log(register);
+        loginForm.reset();
+    };
+
+});
