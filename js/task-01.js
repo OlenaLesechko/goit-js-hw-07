5894b7094c0cddc03a6a249e6cdec5cb7f87ef64
OlenaLesechko <141057041+OlenaLesechko@users.noreply.github.com>
2023-12-08 21:03:06 +0100

    task 1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const allCategories = document.querySelector("ul#categories");
+
+const numberCategories = allCategories.querySelectorAll(".item");
+console.log(`Number of categories: `, numberCategories.length);
+
+numberCategories.forEach((element) => {
+console.log("Category:", element.querySelector("h2").textContent);
+console.log("Elements:", element.querySelectorAll("li").length);
+});
